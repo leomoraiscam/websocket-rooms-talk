d45680843df8f1326a40fec71f7ac3e193bd2be4
lmorais <leozonnn@gmail.com>
2021-06-18 17:18:13 -0300

    use depencies

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import path from 'path';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import mongoose from 'mongoose';
 
 const app = express();
 
 const httpServer = createServer(app);
+
+mongoose.connect("mongodb://localhost/conversations", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
