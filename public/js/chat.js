ccd30f9964d374d3c59bf7fe7b0751a69770fca9
lmorais <leozonnn@gmail.com>
2021-06-18 22:39:34 -0300

    create a new event for emit when connect a new user and populate interface with dynamic datas

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,11 +6,52 @@ function onLoad() {
   const avatar = urlParams.get("avatar");
   const email = urlParams.get("email");
 
+  document.querySelector(".user_logged").innerHTML += `
+    <img
+      class="avatar_user_logged"
+      src=${avatar}
+    />
+    <strong id="user_logged">${name}</strong>
+  `;
+
   socket.emit("start", {
     name, 
     email,
     avatar
   })
+
+  socket.on('new_users', (data) => {
+    const existInDiv = document.getElementById(`user_${data._id}`)
+    
+    if (!existInDiv) { 
+      addUser(data);
+    }
+  });
+
+  socket.emit("get_users", (users) => {
+    users.map((user) => {
+      if (user.email !== email) {
+        addUser(user);
+      }
+    })
+  })
+}
+
+function addUser(user) {
+  const usersList = document.getElementById("users_list");
+  usersList.innerHTML += `
+    <li
+      class="user_name_list"
+      id="user_${user._id}"
+      idUser="${user._id}"
+    >
+      <img
+        class="nav_avatar"
+        src=${user.avatar}
+      />
+      ${user.name}
+    </li>
+  `;
 }
 
 onLoad();
