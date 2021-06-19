7bc78ca40dabab00fb5a285ea0faa4cb48c58347
lmorais <leozonnn@gmail.com>
2021-06-18 21:56:54 -0300

    emit a event to save user

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,16 @@
 const socket = io("http://localhost:3333");
 
-socket.on("welcome", data => {
-  console.log('data', data)
-})
+function onLoad() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const name = urlParams.get("name");
+  const avatar = urlParams.get("avatar");
+  const email = urlParams.get("email");
+
+  socket.emit("start", {
+    name, 
+    email,
+    avatar
+  })
+}
+
+onLoad();
